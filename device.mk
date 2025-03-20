43ba108325617bdf96544fa17f1bd7677f386501
Kai Jones <kvjones0243@gmail.com>
2025-03-19 18:33:11 -0700

    gts7xlwifi: overlay-lineage: Make UDFPS color transparent
    * this fixes the conflict with the onscreen mask
    
    Change-Id: I176058b84c41978a3f3465afb2b46c5e7ac50483

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Sensors
 PRODUCT_PACKAGES += \
