d765d8f8c8484da08b8aa235d9be1b908aa73ff3
programminghoch10 <hoch10@kabelbw.de>
2025-04-01 11:25:50 +0200

    gts7xlwifi: Enable UDFPS long press sensor support
    Change-Id: I665aaab04bac5b7b7cea5e53bcd5fb1f9536d817

--- device.mk
+++ device.mk
@@ -46,6 +46,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 # Sensors
+PRODUCT_PACKAGES += \
+    sensors.samsung
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
