11cfab5ff524792de6f56cc9daf8ff99ef85874e
Bruno Martins <bgcngm@gmail.com>
2025-04-16 01:22:39 +0100

    gts7xlwifi: Configure Pogo keyboard as orientation unaware
    This way the device is configured with a fixed orientation of 0 (no rotation)
    and avoids being mismatched with the applied display rotation of 270.
    
    Fixes dpad keys (no remapping required) and also volume up and down keys,
    which were broken when swapped based on screen orientation.
    
    Partial output of `dumpsys input`:
    
      Device 3: Book Cover Keyboard Slim (EF-DT730)
        EventHub Devices: [ 12 ]
        Generation: 49
        IsExternal: false
        IsWaking: false
        AssociatedDisplayPort: <none>
        AssociatedDisplayUniqueIdByPort: <none>
        AssociatedDisplayUniqueIdByDescriptor: <none>
        HasMic:     false
        Sources: KEYBOARD
        KeyboardType: 2
        ControllerNum: 0
        Keyboard Input Mapper:
          Parameters:
            OrientationAware: false
            HandlesKeyRepeat: false
          Orientation: Rotation0
    
    Change-Id: Ie50994c74391073b8034c2f68cc24513e9c0a08e

--- device.mk
+++ device.mk
@@ -38,7 +38,8 @@ PRODUCT_PACKAGES += \
 
 # Input configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/sec_e-pen.idc
+    $(LOCAL_PATH)/configs/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/sec_e-pen.idc \
+    $(LOCAL_PATH)/configs/idc/Vendor_04e8_Product_a035.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_04e8_Product_a035.idc
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
